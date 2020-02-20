3123e971bccb7bb0ed6f81cabaff192397df15fa
Markku Rossi <mtr@iki.fi>
2020-02-19 20:59:00 -0500

    Unit tests.

---
+++ compiler/ssa/bindings_test.go
@@ -0,0 +1,28 @@
+//
+// Copyright (c) 2020 Markku Rossi
+//
+// All rights reserved.
+//
+
+package ssa
+
+import (
+	"testing"
+)
+
+func TestBindings(t *testing.T) {
+	var a, b Bindings
+
+	a.Set(Variable{
+		Name: "a",
+	})
+	b.Set(Variable{
+		Name: "a",
+	})
+	merged := a.Merge(Variable{
+		Name: "c",
+	}, b)
+	if len(merged) != 1 {
+		t.Errorf("Bindings.Merge failed")
+	}
+}
