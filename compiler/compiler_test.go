1e54537949df56d1f8faf3784916b3d09455d622
Markku Rossi <mtr@iki.fi>
2020-03-02 07:24:36 -0800

    More compiler tests.

--- compiler/compiler_test.go
+++ compiler/compiler_test.go
@@ -10,19 +10,17 @@ import (
 	"testing"
 )
 
-type CompilerTest struct {
+type IteratorTest struct {
 	Name    string
-	Heavy   bool
 	Operand string
 	Bits    int
 	Eval    func(a uint64, b uint64) uint64
 	Code    string
 }
 
-var compilerTests = []CompilerTest{
-	CompilerTest{
+var iteratorTests = []IteratorTest{
+	IteratorTest{
 		Name:    "Add",
-		Heavy:   true,
 		Operand: "+",
 		Bits:    2,
 		Eval: func(a uint64, b uint64) uint64 {
@@ -36,7 +34,7 @@ func main(a, b int3) int3 {
 `,
 	},
 	// 1-bit, 2-bit, and n-bit multipliers have a bit different wiring.
-	CompilerTest{
+	IteratorTest{
 		Name:    "Multiply 1-bit",
 		Operand: "*",
 		Bits:    1,
@@ -50,9 +48,8 @@ func main(a, b int1) int1 {
 }
 `,
 	},
-	CompilerTest{
+	IteratorTest{
 		Name:    "Multiply 2-bits",
-		Heavy:   true,
 		Operand: "*",
 		Bits:    2,
 		Eval: func(a uint64, b uint64) uint64 {
@@ -65,9 +62,8 @@ func main(a, b int4) int4 {
 }
 `,
 	},
-	CompilerTest{
+	IteratorTest{
 		Name:    "Multiply n-bits",
-		Heavy:   true,
 		Operand: "*",
 		Bits:    2,
 		Eval: func(a uint64, b uint64) uint64 {
@@ -82,8 +78,8 @@ func main(a, b int6) int6 {
 	},
 }
 
-func TestCompiler(t *testing.T) {
-	for _, test := range compilerTests {
+func TestIterator(t *testing.T) {
+	for _, test := range iteratorTests {
 		circ, err := Compile(test.Code, &Params{})
 		if err != nil {
 			t.Fatalf("Failed to compile test %s: %s", test.Name, err)
@@ -109,6 +105,90 @@ func TestCompiler(t *testing.T) {
 						expected)
 				}
 			}
+		}
+	}
+}
+
+type FixedTest struct {
+	N1   uint64
+	N2   uint64
+	N3   uint64
+	Zero bool
+	Code string
+}
+
+var fixedTests = []FixedTest{
+	FixedTest{
+		N1: 5,
+		N2: 3,
+		N3: 5,
+		Code: `
+package main
+func main(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
+`,
+	},
+	FixedTest{
+		N1:   5,
+		N2:   3,
+		N3:   6,
+		Zero: true,
+		Code: `
+package main
+func main(a, b int) int {
+    if a > b {
+        return add1(a)
+    }
+    return add1(b)
+}
+func add1(val int) int {
+    return val + 1
+}
+`,
+	},
+	FixedTest{
+		N1:   5,
+		N2:   3,
+		N3:   7,
+		Zero: true,
+		Code: `
+package main
+func main(a, b int) int {
+    if a > b {
+        return add2(a)
+    }
+    return add2(b)
+}
+func add1(val int) int {
+    return val + 1
+}
+func add2(val int) int {
+    return add1(add1(val))
+}
+`,
+	},
+}
+
+func TestFixed(t *testing.T) {
+	for _, test := range fixedTests {
+		circ, err := Compile(test.Code, &Params{})
+		if err != nil {
+			t.Fatalf("Failed to compile test: %s", err)
+		}
+		var n1 = []uint64{test.N1}
+		if test.Zero {
+			n1 = append(n1, 0)
+		}
+		results, err := circ.Compute(n1, []uint64{test.N2})
+		if err != nil {
+			t.Fatalf("compute failed: %s", err)
+		}
+		if results[0] != test.N3 {
+			t.Errorf("test failed: got %d, expected %d", results[0], test.N3)
 		}
 	}
 }
