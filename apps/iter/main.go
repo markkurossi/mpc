cc20d70fd8fc981e2156d900258a5e550607b78b
Markku Rossi <mtr@iki.fi>
2021-03-02 09:04:41 +0200

    Parallel iterator.

--- apps/iter/main.go
+++ apps/iter/main.go
@@ -1,7 +1,7 @@
 //
 // main.go
 //
-// Copyright (c) 2019 Markku Rossi
+// Copyright (c) 2019-2021 Markku Rossi
 //
 // All rights reserved.
 //
@@ -11,6 +11,8 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
+	"strings"
 
 	"github.com/markkurossi/mpc/compiler"
 	"github.com/markkurossi/mpc/compiler/utils"
@@ -24,27 +26,127 @@ func main(a, b int%d) int {
 }
 `
 
+type result struct {
+	bits       int
+	bestLimit  int
+	bestCost   int
+	worstLimit int
+	worstCost  int
+	costs      []int
+}
+
+const (
+	numWorkers = 8
+	startBits  = 8
+)
+
 func main() {
-	for bits := 8; bits < 1024; bits += 8 {
-		code := fmt.Sprintf(template, bits)
-		var bestLimit int
-		var bestCost int
+	results := make(map[int]*result)
+	ch := make(chan *result)
 
-		params := utils.NewParams()
+	for i := 0; i < numWorkers; i++ {
+		go func(bits int) {
+			for ; ; bits += numWorkers {
+				code := fmt.Sprintf(template, bits)
+				var bestLimit int
+				var bestCost int
+				var worstLimit int
+				var worstCost int
+				var costs []int
 
-		for limit := 4; limit < 64; limit += 2 {
-			params.CircMultArrayTreshold = limit
-			circ, _, err := compiler.New(params).Compile(code)
-			if err != nil {
-				log.Fatalf("Compilation %d:%d failed: %s", bits, limit, err)
+				params := utils.NewParams()
+
+				for limit := 8; limit <= 22; limit++ {
+					params.CircMultArrayTreshold = limit
+					circ, _, err := compiler.New(params).Compile(code)
+					if err != nil {
+						log.Fatalf("Compilation %d:%d failed: %s\n%s",
+							bits, limit, err, code)
+					}
+					cost := circ.Cost()
+					costs = append(costs, cost)
+
+					if bestCost == 0 || cost < bestCost {
+						bestCost = cost
+						bestLimit = limit
+					}
+					if cost > worstCost {
+						worstCost = cost
+						worstLimit = limit
+					}
+				}
+				ch <- &result{
+					bits:       bits,
+					bestLimit:  bestLimit,
+					bestCost:   bestCost,
+					worstLimit: worstLimit,
+					worstCost:  worstCost,
+					costs:      costs,
+				}
 			}
-			cost := circ.Cost()
-
-			if bestCost == 0 || cost < bestCost {
-				bestCost = cost
-				bestLimit = limit
-			}
-		}
-		fmt.Printf("%d\t%d\t%d\n", bits, bestLimit, bestCost)
+		}(startBits + i)
 	}
+
+	next := startBits
+
+	for result := range ch {
+		results[result.bits] = result
+		for {
+			r, ok := results[next]
+			if !ok {
+				break
+			}
+			if r.bestLimit == 19 {
+				fmt.Printf("\t// %d: %d,\t%d\t%.4f\t%s\n",
+					r.bits, r.bestLimit,
+					r.bestCost, float64(r.bestCost)/float64(r.worstCost),
+					Sparkline(r.costs))
+			} else {
+				fmt.Printf("\t%d: %d, //\t%d\t%.4f\t%s\n",
+					r.bits, r.bestLimit,
+					r.bestCost, float64(r.bestCost)/float64(r.worstCost),
+					Sparkline(r.costs))
+			}
+			next++
+		}
+	}
+}
+
+// Sparkline creates a histogram chart of values. The chart is scaled
+// to [min...max] containing differences between values.
+func Sparkline(values []int) string {
+	if len(values) == 0 {
+		return ""
+	}
+
+	min := math.MaxInt32
+	max := math.MinInt32
+
+	for _, v := range values {
+		if v < min {
+			min = v
+		}
+		if v > max {
+			max = v
+		}
+	}
+	delta := max - min
+
+	var sb strings.Builder
+	for _, v := range values {
+		var tick int
+		if delta == 0 {
+			tick = 4
+		} else {
+			tick = (v - min) * 7 / delta
+		}
+		if v == min && false {
+			sb.WriteString("\x1b[92m")
+			sb.WriteRune(rune(0x2581 + tick))
+			sb.WriteString("\x1b[0m")
+		} else {
+			sb.WriteRune(rune(0x2581 + tick))
+		}
+	}
+	return sb.String()
 }
