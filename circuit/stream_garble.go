0adb6ae09c3fb1467d01de5bad6a67411208aab8
Markku Rossi <mtr@iki.fi>
2022-01-26 07:11:22 +0100

    Refactored gate serialization.

--- circuit/stream_garble.go
+++ circuit/stream_garble.go
@@ -434,6 +434,12 @@ func (stream *Streaming) garbleGate(g *Gate, idp *uint32,
 	table = table[0:4]
 	var tableStart, tableCount, wireCount int
 
+	if g.Op == INV {
+		wireCount = 2
+	} else {
+		wireCount = 3
+	}
+
 	// Inputs.
 	switch g.Op {
 	case XOR, XNOR, AND, OR:
@@ -447,179 +453,17 @@ func (stream *Streaming) garbleGate(g *Gate, idp *uint32,
 		return fmt.Errorf("invalid gate type %s", g.Op)
 	}
 
-	// Output.
-	switch g.Op {
-	case XOR:
-		l0 := a.L0
-		l0.Xor(b.L0)
-
-		l1 := l0
-		l1.Xor(stream.r)
-		c = ot.Wire{
-			L0: l0,
-			L1: l1,
-		}
-
-	case XNOR:
-		l0 := a.L0
-		l0.Xor(b.L0)
-
-		l1 := l0
-		l1.Xor(stream.r)
-		c = ot.Wire{
-			L0: l1,
-			L1: l0,
-		}
-
-	case AND:
-		pa := a.L0.S()
-		pb := b.L0.S()
-
-		j0 := *idp
-		j1 := *idp + 1
-		*idp = *idp + 2
-
-		// First half gate.
-		tg := encryptHalf(stream.alg, a.L0, j0, data)
-		tg.Xor(encryptHalf(stream.alg, a.L1, j0, data))
-		if pb {
-			tg.Xor(stream.r)
-		}
-		wg0 := encryptHalf(stream.alg, a.L0, j0, data)
-		if pa {
-			wg0.Xor(tg)
-		}
-
-		// Second half gate.
-		te := encryptHalf(stream.alg, b.L0, j1, data)
-		te.Xor(encryptHalf(stream.alg, b.L1, j1, data))
-		te.Xor(a.L0)
-		we0 := encryptHalf(stream.alg, b.L0, j1, data)
-		if pb {
-			we0.Xor(te)
-			we0.Xor(a.L0)
-		}
-
-		// Combine halves
-		l0 := wg0
-		l0.Xor(we0)
-
-		l1 := l0
-		l1.Xor(stream.r)
-
-		c = ot.Wire{
-			L0: l0,
-			L1: l1,
-		}
-		table[0] = tg
-		table[1] = te
-		tableCount = 2
-
-	case OR, INV:
-		// Row reduction creates labels below so that the first row is
-		// all zero.
-
-	default:
-		panic("invalid gate type")
-	}
-
-	switch g.Op {
-	case XOR, XNOR:
-		// Free XOR.
-		wireCount = 3
-
-	case AND:
-		// Half AND garbled above.
-		wireCount = 3
-
-	case OR:
-		// a b c
-		// -----
-		// 0 0 0
-		// 0 1 1
-		// 1 0 1
-		// 1 1 1
-		id := *idp
-		*idp = *idp + 1
-		table[idx(a.L0, b.L0)] = encrypt(stream.alg, a.L0, b.L0, c.L0, id, data)
-		table[idx(a.L0, b.L1)] = encrypt(stream.alg, a.L0, b.L1, c.L1, id, data)
-		table[idx(a.L1, b.L0)] = encrypt(stream.alg, a.L1, b.L0, c.L1, id, data)
-		table[idx(a.L1, b.L1)] = encrypt(stream.alg, a.L1, b.L1, c.L1, id, data)
-
-		// Row reduction. Make first table all zero so we don't have
-		// to transmit it.
-
-		l0Index := idx(a.L0, b.L0)
-
-		c.L0 = table[0]
-		c.L1 = table[0]
-
-		if l0Index == 0 {
-			c.L1.Xor(stream.r)
-		} else {
-			c.L0.Xor(stream.r)
-		}
-		for i := 0; i < 4; i++ {
-			if i == l0Index {
-				table[i].Xor(c.L0)
-			} else {
-				table[i].Xor(c.L1)
-			}
-		}
-
-		tableStart = 1
-		tableCount = 3
-		wireCount = 3
-
-	case INV:
-		// a b c
-		// -----
-		// 0   1
-		// 1   0
-		zero := ot.Label{}
-		id := *idp
-		*idp = *idp + 1
-		table[idxUnary(a.L0)] = encrypt(stream.alg, a.L0, zero, c.L1, id, data)
-		table[idxUnary(a.L1)] = encrypt(stream.alg, a.L1, zero, c.L0, id, data)
-
-		l0Index := idxUnary(a.L0)
-
-		c.L0 = table[0]
-		c.L1 = table[0]
-
-		if l0Index == 0 {
-			c.L0.Xor(stream.r)
-		} else {
-			c.L1.Xor(stream.r)
-		}
-		for i := 0; i < 2; i++ {
-			if i == l0Index {
-				table[i].Xor(c.L1)
-			} else {
-				table[i].Xor(c.L0)
-			}
-		}
-
-		tableStart = 1
-		tableCount = 1
-		wireCount = 2
-
-	default:
-		return fmt.Errorf("invalid operand %s", g.Op)
-	}
-
+	// Output wire.
 	if g.Output < stream.firstTmp {
 		cIndex = stream.in[g.Output]
-		stream.wires[cIndex] = c
 	} else if g.Output >= stream.firstOut {
 		cIndex = stream.out[g.Output-stream.firstOut]
-		stream.wires[cIndex] = c
 	} else {
 		cIndex = g.Output
 		cTmp = true
-		stream.tmp[g.Output] = c
 	}
 
+	// Serialize opcode and wires.
 	op := byte(g.Op)
 	if aTmp {
 		op |= 0b10000000
@@ -709,6 +553,169 @@ func (stream *Streaming) garbleGate(g *Gate, idp *uint32,
 		default:
 			panic(fmt.Sprintf("invalid wire count: %d", wireCount))
 		}
+	}
+
+	// Output.
+	switch g.Op {
+	case XOR:
+		l0 := a.L0
+		l0.Xor(b.L0)
+
+		l1 := l0
+		l1.Xor(stream.r)
+		c = ot.Wire{
+			L0: l0,
+			L1: l1,
+		}
+
+	case XNOR:
+		l0 := a.L0
+		l0.Xor(b.L0)
+
+		l1 := l0
+		l1.Xor(stream.r)
+		c = ot.Wire{
+			L0: l1,
+			L1: l0,
+		}
+
+	case AND:
+		pa := a.L0.S()
+		pb := b.L0.S()
+
+		j0 := *idp
+		j1 := *idp + 1
+		*idp = *idp + 2
+
+		// First half gate.
+		tg := encryptHalf(stream.alg, a.L0, j0, data)
+		tg.Xor(encryptHalf(stream.alg, a.L1, j0, data))
+		if pb {
+			tg.Xor(stream.r)
+		}
+		wg0 := encryptHalf(stream.alg, a.L0, j0, data)
+		if pa {
+			wg0.Xor(tg)
+		}
+
+		// Second half gate.
+		te := encryptHalf(stream.alg, b.L0, j1, data)
+		te.Xor(encryptHalf(stream.alg, b.L1, j1, data))
+		te.Xor(a.L0)
+		we0 := encryptHalf(stream.alg, b.L0, j1, data)
+		if pb {
+			we0.Xor(te)
+			we0.Xor(a.L0)
+		}
+
+		// Combine halves
+		l0 := wg0
+		l0.Xor(we0)
+
+		l1 := l0
+		l1.Xor(stream.r)
+
+		c = ot.Wire{
+			L0: l0,
+			L1: l1,
+		}
+		table[0] = tg
+		table[1] = te
+		tableCount = 2
+
+	case OR, INV:
+		// Row reduction creates labels below so that the first row is
+		// all zero.
+
+	default:
+		panic("invalid gate type")
+	}
+
+	switch g.Op {
+	case XOR, XNOR:
+		// Free XOR.
+
+	case AND:
+		// Half AND garbled above.
+
+	case OR:
+		// a b c
+		// -----
+		// 0 0 0
+		// 0 1 1
+		// 1 0 1
+		// 1 1 1
+		id := *idp
+		*idp = *idp + 1
+		table[idx(a.L0, b.L0)] = encrypt(stream.alg, a.L0, b.L0, c.L0, id, data)
+		table[idx(a.L0, b.L1)] = encrypt(stream.alg, a.L0, b.L1, c.L1, id, data)
+		table[idx(a.L1, b.L0)] = encrypt(stream.alg, a.L1, b.L0, c.L1, id, data)
+		table[idx(a.L1, b.L1)] = encrypt(stream.alg, a.L1, b.L1, c.L1, id, data)
+
+		// Row reduction. Make first table all zero so we don't have
+		// to transmit it.
+
+		l0Index := idx(a.L0, b.L0)
+
+		c.L0 = table[0]
+		c.L1 = table[0]
+
+		if l0Index == 0 {
+			c.L1.Xor(stream.r)
+		} else {
+			c.L0.Xor(stream.r)
+		}
+		for i := 0; i < 4; i++ {
+			if i == l0Index {
+				table[i].Xor(c.L0)
+			} else {
+				table[i].Xor(c.L1)
+			}
+		}
+
+		tableStart = 1
+		tableCount = 3
+
+	case INV:
+		// a b c
+		// -----
+		// 0   1
+		// 1   0
+		zero := ot.Label{}
+		id := *idp
+		*idp = *idp + 1
+		table[idxUnary(a.L0)] = encrypt(stream.alg, a.L0, zero, c.L1, id, data)
+		table[idxUnary(a.L1)] = encrypt(stream.alg, a.L1, zero, c.L0, id, data)
+
+		l0Index := idxUnary(a.L0)
+
+		c.L0 = table[0]
+		c.L1 = table[0]
+
+		if l0Index == 0 {
+			c.L0.Xor(stream.r)
+		} else {
+			c.L1.Xor(stream.r)
+		}
+		for i := 0; i < 2; i++ {
+			if i == l0Index {
+				table[i].Xor(c.L1)
+			} else {
+				table[i].Xor(c.L0)
+			}
+		}
+
+		tableStart = 1
+		tableCount = 1
+
+	default:
+		return fmt.Errorf("invalid operand %s", g.Op)
+	}
+
+	if cTmp {
+		stream.tmp[cIndex] = c
+	} else {
+		stream.wires[cIndex] = c
 	}
 
 	for i := 0; i < tableCount; i++ {
