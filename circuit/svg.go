7eab169700a12fd587f6e1d6167c225da2653445
Markku Rossi <mtr@iki.fi>
2022-05-07 10:46:37 +0200

    Rendering constant wires 0 and 1.

--- circuit/svg.go
+++ circuit/svg.go
@@ -29,9 +29,57 @@ type point struct {
 	x, y float64
 }
 
+type wireType int
+
+const (
+	wireTypeNormal wireType = iota
+	wireTypeZero
+	wireTypeOne
+)
+
 type wire struct {
+	t    wireType
 	from point
 	to   point
+}
+
+func (w *wire) svg(out io.Writer) {
+	label := "1"
+
+	switch w.t {
+	case wireTypeNormal:
+		fmt.Fprintf(out, `<path d="M %v %v C %v %v %v %v %v %v" />
+`,
+			w.from.x, w.from.y,
+			w.from.x, w.from.y+10,
+			w.to.x, w.to.y-10,
+			w.to.x, w.to.y)
+
+	case wireTypeZero:
+		label = "0"
+		fallthrough
+
+	case wireTypeOne:
+		fmt.Fprintf(out, `    <g fill="#000">
+      <text x="%v" y="%v" text-anchor="middle">%v</text>
+    </g>
+`,
+			w.to.x, w.to.y-2, label)
+	}
+}
+
+type svgCtx struct {
+	wireStarts []point
+	zero       Wire
+	one        Wire
+}
+
+func (ctx *svgCtx) setWireType(input Wire, w *wire) {
+	if input == ctx.zero {
+		w.t = wireTypeZero
+	} else if input == ctx.one {
+		w.t = wireTypeOne
+	}
 }
 
 func (c *Circuit) Svg(out io.Writer) {
@@ -84,34 +132,36 @@ func (c *Circuit) Svg(out io.Writer) {
 
 	// Render circuit.
 
-	wireStarts := make([]point, c.NumWires)
+	ctx := &svgCtx{
+		wireStarts: make([]point, c.NumWires),
+	}
 
 	fmt.Fprintf(out,
 		`<svg xmlns="http://www.w3.org/2000/svg" width="%d" height="%d">
+  <style><![CDATA[
+  text {
+    font: 10px Verdana, Helvetica, Arial, sans-serif;
+  }
+  ]]></style>
   <g fill="none" stroke="#000" stroke-width=".5">
 `,
 		width*(gateWidth+gatePadX), height*(gateHeight+gatePadY))
 
-	var wires []wire
+	var wires []*wire
 
 	for _, tile := range tiles {
-		wires = append(wires, tile.gate.Svg(out, tile.x, tile.y, wireStarts)...)
+		wires = append(wires, tile.gate.svg(out, tile.x, tile.y, ctx)...)
 	}
 
 	for _, w := range wires {
-		fmt.Fprintf(out, `<path d="M %v %v C %v %v %v %v %v %v" />
-`,
-			w.from.x, w.from.y,
-			w.from.x, w.from.y+10,
-			w.to.x, w.to.y-10,
-			w.to.x, w.to.y)
+		w.svg(out)
 	}
 
 	fmt.Fprintln(out, "  </g>\n</svg>")
 }
 
-func (g *Gate) Svg(out io.Writer, x, y float64, wireStarts []point) []wire {
-	fmt.Fprintf(out, `<g transform="translate(%v %v)">
+func (g *Gate) svg(out io.Writer, x, y float64, ctx *svgCtx) []*wire {
+	fmt.Fprintf(out, `    <g transform="translate(%v %v)">
 `,
 		x, y)
 
@@ -120,37 +170,64 @@ func (g *Gate) Svg(out io.Writer, x, y float64, wireStarts []point) []wire {
 		tmpl = templates[Count]
 	}
 	out.Write([]byte(tmpl.Expand()))
-	fmt.Fprintln(out, "</g>")
+	fmt.Fprintln(out, `    </g>`)
 
-	wireStarts[g.Output] = point{
+	ctx.wireStarts[g.Output] = point{
 		x: x + gateWidth/2,
 		y: y + gateHeight,
 	}
 
-	var wires []wire
+	var wires []*wire
 
 	switch g.Op {
 	case XOR, XNOR, AND, OR:
-		wires = append(wires, wire{
-			from: wireStarts[g.Input1],
+		wire := &wire{
+			from: ctx.wireStarts[g.Input1],
 			to: point{
 				x: x + intCvt(65),
 				y: y,
 			},
-		})
+		}
+		ctx.setWireType(g.Input1, wire)
+		wires = append(wires, wire)
 		fallthrough
 
 	case INV:
-		wires = append(wires, wire{
-			from: wireStarts[g.Input0],
+		wire := &wire{
+			from: ctx.wireStarts[g.Input0],
 			to: point{
 				x: x + intCvt(35),
 				y: y,
 			},
-		})
+		}
+		ctx.setWireType(g.Input0, wire)
+		wires = append(wires, wire)
+	}
+
+	// Constant value gates.
+	switch g.Op {
+	case XOR:
+		if g.Input0 == g.Input1 {
+			ctx.zero = g.Output
+			g.staticOutput(out, x, y, "0")
+		}
+
+	case XNOR:
+		if g.Input0 == g.Input1 {
+			ctx.one = g.Output
+			g.staticOutput(out, x, y, "1")
+		}
 	}
 
 	return wires
+}
+
+func (g *Gate) staticOutput(out io.Writer, x, y float64, label string) {
+	fmt.Fprintf(out, `    <g fill="#000">
+      <text x="%v" y="%v" text-anchor="middle">%v</text>
+    </g>
+`,
+		x+gateWidth/2, y+gateHeight+10, label)
 }
 
 func scale(in int) float64 {
@@ -167,35 +244,35 @@ var intCvt IntCvt
 var floatCvt FloatCvt
 
 func init() {
-	templates[XOR] = NewTemplate(`<path
-        d="M {{25}} {{20}}
-           c {{10}} {{10}} {{40}} {{10}} {{50}} 0" />
-  <path
-        d="M {{25}} {{25}}
-           c {{10}} {{10}} {{40}} {{10}} {{50}} 0" />
+	templates[XOR] = NewTemplate(`      <path
+          d="M {{25}} {{20}}
+             c {{10}} {{10}} {{40}} {{10}} {{50}} 0" />
+      <path
+          d="M {{25}} {{25}}
+             c {{10}} {{10}} {{40}} {{10}} {{50}} 0" />
 
-  <path
-        d="M {{75}} {{25}}
-           v {{25}}
-           s 0 {{10}} {{-25}} {{25}}" />
-  <path
-        d="M {{25}} {{25}}
-           v {{25}}
-           s 0 {{10}} {{25}} {{25}}" />
+      <path
+          d="M {{75}} {{25}}
+             v {{25}}
+             s 0 {{10}} {{-25}} {{25}}" />
+      <path
+          d="M {{25}} {{25}}
+             v {{25}}
+             s 0 {{10}} {{25}} {{25}}" />
 
-  <!-- Wires -->
-  <path
-        d="M {{35}} 0
-           v {{25}}
-           z" />
-  <path
-        d="M {{65}} 0
-           v {{25}}
-           z" />
-  <path
-        d="M {{50}} {{75}}
-           v {{25}}
-           z" />
+      <!-- Wires -->
+      <path
+          d="M {{35}} 0
+             v {{25}}
+             z" />
+      <path
+          d="M {{65}} 0
+             v {{25}}
+             z" />
+      <path
+          d="M {{50}} {{75}}
+             v {{25}}
+             z" />
 `)
 	templates[XNOR] = NewTemplate(`    <path
           d="M {{25}} {{20}}
