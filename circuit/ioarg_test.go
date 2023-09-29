a4cb8d66ffd54dce8da78404eeba2be2524ce801
Markku Rossi <mtr@iki.fi>
2023-09-29 13:41:33 +0200

    Added tests.

---
+++ circuit/ioarg_test.go
@@ -0,0 +1,54 @@
+//
+// Copyright (c) 2023 Markku Rossi
+//
+// All rights reserved.
+//
+
+package circuit
+
+import (
+	"testing"
+)
+
+var inputSizeTests = []struct {
+	inputs []string
+	sizes  []int
+}{
+	{
+		inputs: []string{
+			"0", "f", "false", "1", "t", "true",
+		},
+		sizes: []int{
+			1, 1, 1, 1, 1, 1,
+		},
+	},
+	{
+		inputs: []string{
+			"0xdeadbeef", "255",
+		},
+		sizes: []int{
+			32, 8,
+		},
+	},
+}
+
+func TestInputSizes(t *testing.T) {
+	for idx, test := range inputSizeTests {
+		sizes, err := InputSizes(test.inputs)
+		if err != nil {
+			t.Errorf("t%v: InputSizes(%v) failed: %v", idx, test.inputs, err)
+			continue
+		}
+		if len(sizes) != len(test.sizes) {
+			t.Errorf("t%v: unexpected # of sizes: got %v, expected %v",
+				idx, len(sizes), len(test.sizes))
+			continue
+		}
+		for i := 0; i < len(sizes); i++ {
+			if sizes[i] != test.sizes[i] {
+				t.Errorf("t%v: sizes[%v]=%v, expected %v",
+					idx, i, sizes[i], test.sizes[i])
+			}
+		}
+	}
+}
