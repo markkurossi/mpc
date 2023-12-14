871672d44fab8636973f7d857c45f8d31980ba0e
Markku Rossi <mtr@iki.fi>
2023-12-14 11:45:40 +0100

    Traverse test directory with filepath.WalkDir.

--- compiler/testsuite_test.go
+++ compiler/testsuite_test.go
@@ -8,9 +8,10 @@ package compiler
 
 import (
 	"fmt"
+	"io/fs"
 	"math/big"
 	"os"
-	"path"
+	"path/filepath"
 	"regexp"
 	"runtime/pprof"
 	"strings"
@@ -30,150 +31,149 @@ var (
 )
 
 func TestSuite(t *testing.T) {
-	dir, err := os.Open(testsuite)
-	if err != nil {
-		t.Fatalf("Failed to open tests directory: %s", err)
-	}
-	defer dir.Close()
-
-	files, err := dir.Readdirnames(-1)
-	if err != nil {
-		t.Fatalf("Failed to list tests directory: %s", err)
-	}
-
 	params := utils.NewParams()
 	params.MPCLCErrorLoc = true
 	compiler := New(params)
 
-loop:
-	for _, file := range files {
-		if !strings.HasSuffix(file, ".mpcl") {
+	filepath.WalkDir(testsuite,
+		func(path string, d fs.DirEntry, err error) error {
+			if err != nil {
+				return err
+			}
+			if d.IsDir() {
+				return nil
+			}
+			fmt.Printf("WalkDir: %s\n", path)
+			testFile(t, compiler, path)
+			return nil
+		})
+}
+
+func testFile(t *testing.T, compiler *Compiler, file string) {
+	if !strings.HasSuffix(file, ".mpcl") {
+		return
+	}
+	pkg, err := compiler.ParseFile(file)
+	if err != nil {
+		t.Errorf("failed to parse '%s': %s", file, err)
+		return
+	}
+	main, ok := pkg.Functions["main"]
+	if !ok {
+		t.Errorf("%s: no main function", file)
+	}
+
+	var cpuprof bool
+	var prof *os.File
+	var lsb bool
+	base := 10
+
+	for _, annotation := range main.Annotations {
+		ann := strings.TrimSpace(annotation)
+		if strings.HasPrefix(ann, "@heavy") && testing.Short() {
+			fmt.Printf("Skipping heavy test %s\n", file)
+			return
+		}
+		if strings.HasPrefix(ann, "@pprof") {
+			cpuprof = true
+			prof, err = os.Create(fmt.Sprintf("%s.cpu.prof", file))
+			if err != nil {
+				t.Errorf("%s: failed to create cpu.prof: %s", file, err)
+				return
+			}
+			err = pprof.StartCPUProfile(prof)
+			if err != nil {
+				t.Errorf("%s: failed to start CPU profile: %s", file, err)
+				return
+			}
 			continue
 		}
-		name := path.Join(testsuite, file)
-		pkg, err := compiler.ParseFile(name)
+		if strings.HasPrefix(ann, "@Hex") {
+			base = 16
+			continue
+		}
+		if strings.HasPrefix(ann, "@LSB") {
+			lsb = true
+			continue
+		}
+		if !strings.HasPrefix(ann, "@Test ") {
+			continue
+		}
+		parts := reWhitespace.Split(ann, -1)
+
+		var inputValues []string
+		var inputs []*big.Int
+		var outputs []*big.Int
+		var sep bool
+
+		for i := 1; i < len(parts); i++ {
+			part := parts[i]
+			if part == "=" {
+				sep = true
+				continue
+			}
+			v := new(big.Int)
+			if base == 16 && lsb {
+				part = reverse(part)
+			}
+
+			_, ok := v.SetString(part, 0)
+			if !ok {
+				t.Errorf("%s: invalid argument '%s'", file, part)
+				return
+			}
+			if sep {
+				outputs = append(outputs, v)
+			} else {
+				inputValues = append(inputValues, part)
+				inputs = append(inputs, v)
+			}
+		}
+		var inputSizes [][]int
+		for _, iv := range inputValues {
+			sizes, err := circuit.InputSizes([]string{iv})
+			if err != nil {
+				t.Errorf("%s: invalid inputs: %s", file, err)
+				return
+			}
+			inputSizes = append(inputSizes, sizes)
+		}
+		circ, _, err := compiler.CompileFile(file, inputSizes)
 		if err != nil {
-			t.Errorf("failed to parse '%s': %s", file, err)
-			continue
-		}
-		main, ok := pkg.Functions["main"]
-		if !ok {
-			t.Errorf("%s: no main function", file)
-			continue
+			t.Errorf("failed to compile '%s': %s", file, err)
+			return
 		}
 
-		var cpuprof bool
-		var prof *os.File
-		var lsb bool
-		base := 10
+		results, err := circ.Compute(inputs)
+		if err != nil {
+			t.Errorf("%s: compute failed: %s", file, err)
+			return
+		}
 
-		for _, annotation := range main.Annotations {
-			ann := strings.TrimSpace(annotation)
-			if strings.HasPrefix(ann, "@heavy") && testing.Short() {
-				fmt.Printf("Skipping heavy test %s\n", file)
-				continue loop
+		if len(results) != len(outputs) {
+			t.Errorf("%s: unexpected return values: got %v, expected %v",
+				file, results, outputs)
+			return
+		}
+		for idx, result := range results {
+			out := circ.Outputs[idx]
+			bits := int(out.Type.Bits)
+			if out.Type.Type == types.TInt && result.Bit(bits-1) == 1 {
+				// Negative number.
+				tmp := new(big.Int)
+				tmp.SetBit(tmp, bits, 1)
+				result.Sub(tmp, result)
+				result.Neg(result)
 			}
-			if strings.HasPrefix(ann, "@pprof") {
-				cpuprof = true
-				prof, err = os.Create(fmt.Sprintf("%s.cpu.prof", file))
-				if err != nil {
-					t.Errorf("%s: failed to create cpu.prof: %s", file, err)
-					continue loop
-				}
-				err = pprof.StartCPUProfile(prof)
-				if err != nil {
-					t.Errorf("%s: failed to start CPU profile: %s", file, err)
-					continue loop
-				}
-				continue
-			}
-			if strings.HasPrefix(ann, "@Hex") {
-				base = 16
-				continue
-			}
-			if strings.HasPrefix(ann, "@LSB") {
-				lsb = true
-				continue
-			}
-			if !strings.HasPrefix(ann, "@Test ") {
-				continue
-			}
-			parts := reWhitespace.Split(ann, -1)
-
-			var inputValues []string
-			var inputs []*big.Int
-			var outputs []*big.Int
-			var sep bool
-
-			for i := 1; i < len(parts); i++ {
-				part := parts[i]
-				if part == "=" {
-					sep = true
-					continue
-				}
-				v := new(big.Int)
-				if base == 16 && lsb {
-					part = reverse(part)
-				}
-
-				_, ok := v.SetString(part, 0)
-				if !ok {
-					t.Errorf("%s: invalid argument '%s'", file, part)
-					continue loop
-				}
-				if sep {
-					outputs = append(outputs, v)
-				} else {
-					inputValues = append(inputValues, part)
-					inputs = append(inputs, v)
-				}
-			}
-			var inputSizes [][]int
-			for _, iv := range inputValues {
-				sizes, err := circuit.InputSizes([]string{iv})
-				if err != nil {
-					t.Errorf("%s: invalid inputs: %s", file, err)
-					continue loop
-				}
-				inputSizes = append(inputSizes, sizes)
-			}
-			circ, _, err := compiler.CompileFile(name, inputSizes)
-			if err != nil {
-				t.Errorf("failed to compile '%s': %s", file, err)
-				continue loop
-			}
-
-			results, err := circ.Compute(inputs)
-			if err != nil {
-				t.Errorf("%s: compute failed: %s", file, err)
-				continue loop
-			}
-
-			if len(results) != len(outputs) {
-				t.Errorf("%s: unexpected return values: got %v, expected %v",
-					file, results, outputs)
-				continue loop
-			}
-			for idx, result := range results {
-				out := circ.Outputs[idx]
-				bits := int(out.Type.Bits)
-				if out.Type.Type == types.TInt && result.Bit(bits-1) == 1 {
-					// Negative number.
-					tmp := new(big.Int)
-					tmp.SetBit(tmp, bits, 1)
-					result.Sub(tmp, result)
-					result.Neg(result)
-				}
-				if result.Cmp(outputs[idx]) != 0 {
-					t.Errorf("%s: result %d mismatch: got %v, expected %v",
-						file, idx, result.Text(base), outputs[idx].Text(base))
-				}
+			if result.Cmp(outputs[idx]) != 0 {
+				t.Errorf("%s: result %d mismatch: got %v, expected %v",
+					file, idx, result.Text(base), outputs[idx].Text(base))
 			}
 		}
-		if cpuprof {
-			pprof.StopCPUProfile()
-			prof.Close()
-		}
+	}
+	if cpuprof {
+		pprof.StopCPUProfile()
+		prof.Close()
 	}
 }
 
