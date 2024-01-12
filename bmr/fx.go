d20524b4be9d42b35c838681aa74ed9c445176be
Markku Rossi <mtr@iki.fi>
2024-01-12 08:08:25 +0100

    Fx() and Fxk() secure multiplication.

--- bmr/fx.go
+++ bmr/fx.go
@@ -12,7 +12,49 @@ import (
 
 // FxSend implements the sender part of the secure multiplication
 // algorithm Fx (3.1.1 Secure Multiplication - page 7).
-func FxSend(oti ot.OT, a Label) (r Label, err error) {
+func FxSend(oti ot.OT, a uint) (r uint, err error) {
+	rl, err := NewLabel()
+	if err != nil {
+		return 0, err
+	}
+
+	x0 := rl
+	x1 := rl
+
+	var al Label
+	al[0] = byte(a)
+	x1.Xor(al)
+
+	wire := ot.Wire{
+		L0: x0.ToOT(),
+		L1: x1.ToOT(),
+	}
+
+	err = oti.Send([]ot.Wire{wire})
+	if err != nil {
+		return 0, err
+	}
+	return uint(rl[0] & 1), nil
+}
+
+// FxReceive implements the receiver part of the secure multiplication
+// algorithm Fx (3.1.1 Secure Multiplication - page 7).
+func FxReceive(oti ot.OT, b uint) (xb uint, err error) {
+	flags := []bool{b == 1}
+	var result [1]ot.Label
+
+	err = oti.Receive(flags, result[:])
+	if err != nil {
+		return 0, err
+	}
+	var xl Label
+	xl.FromOT(result[0])
+	return uint(xl[0] & 1), nil
+}
+
+// FxkSend implements the sender part of the secure multiplication
+// algorithm Fx (3.1.1 Secure Multiplication - page 7).
+func FxkSend(oti ot.OT, s Label) (r Label, err error) {
 	r, err = NewLabel()
 	if err != nil {
 		return
@@ -20,7 +62,7 @@ func FxSend(oti ot.OT, a Label) (r Label, err error) {
 
 	x0 := r
 	x1 := r
-	x1.Xor(a)
+	x1.Xor(s)
 
 	wire := ot.Wire{
 		L0: x0.ToOT(),
@@ -31,10 +73,10 @@ func FxSend(oti ot.OT, a Label) (r Label, err error) {
 	return
 }
 
-// FxReceive implements the receiver part of the secure multiplication
+// FxkReceive implements the receiver part of the secure multiplication
 // algorithm Fx (3.1.1 Secure Multiplication - page 7).
-func FxReceive(oti ot.OT, bit bool) (xb Label, err error) {
-	flags := []bool{bit}
+func FxkReceive(oti ot.OT, b uint) (xb Label, err error) {
+	flags := []bool{b == 1}
 	var result [1]ot.Label
 
 	err = oti.Receive(flags, result[:])
--- bmr/fx_test.go
+++ bmr/fx_test.go
@@ -13,11 +13,13 @@ import (
 )
 
 func TestFx(t *testing.T) {
-	testFx(t, false)
-	testFx(t, true)
+	testFx(t, 0, 0)
+	testFx(t, 0, 1)
+	testFx(t, 1, 0)
+	testFx(t, 1, 1)
 }
 
-func testFx(t *testing.T, b bool) {
+func testFx(t *testing.T, a, b uint) {
 	fp, tp := ot.NewPipe()
 
 	ch := make(chan interface{})
@@ -30,10 +32,6 @@ func testFx(t *testing.T, b bool) {
 		t.Fatal(err)
 	}
 
-	a, err := NewLabel()
-	if err != nil {
-		t.Fatal(err)
-	}
 	r, err := FxSend(oti, a)
 	if err != nil {
 		t.Fatal(err)
@@ -44,8 +42,78 @@ func testFx(t *testing.T, b bool) {
 	case error:
 		t.Fatal(xb)
 
+	case uint:
+		if b == 1 {
+			// b = 1 implies that xb = x1 = r⊕a
+			tst := r ^ a
+			if tst != xb {
+				t.Errorf("b=1, got %v, expected %v\n", xb, tst)
+			}
+		} else {
+			// b = 0 implies that xb = x0 = r
+			if xb != r {
+				t.Errorf("b=0: got %v, expected %v\n", xb, r)
+			}
+		}
+
+	default:
+		t.Fatalf("unexpected result: %v(%T)", ret, ret)
+	}
+}
+
+func fxReceiver(pipe ot.IO, ch chan interface{}, b uint) {
+	defer close(ch)
+
+	oti := ot.NewCO()
+	err := oti.InitReceiver(pipe)
+	if err != nil {
+		ch <- err
+		return
+	}
+
+	xb, err := FxReceive(oti, b)
+	if err != nil {
+		ch <- err
+		return
+	}
+
+	ch <- xb
+}
+
+func TestFxk(t *testing.T) {
+	testFxk(t, 0)
+	testFxk(t, 1)
+}
+
+func testFxk(t *testing.T, b uint) {
+	fp, tp := ot.NewPipe()
+
+	ch := make(chan interface{})
+
+	go fxkReceiver(tp, ch, b)
+
+	oti := ot.NewCO()
+	err := oti.InitSender(fp)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	a, err := NewLabel()
+	if err != nil {
+		t.Fatal(err)
+	}
+	r, err := FxkSend(oti, a)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ret := <-ch
+	switch xb := ret.(type) {
+	case error:
+		t.Fatal(xb)
+
 	case Label:
-		if b {
+		if b == 1 {
 			// b = 1 implies that xb = x1 = r⊕a
 			tst := r
 			tst.Xor(a)
@@ -64,7 +132,7 @@ func testFx(t *testing.T, b bool) {
 	}
 }
 
-func fxReceiver(pipe ot.IO, ch chan interface{}, bit bool) {
+func fxkReceiver(pipe ot.IO, ch chan interface{}, b uint) {
 	defer close(ch)
 
 	oti := ot.NewCO()
@@ -74,7 +142,7 @@ func fxReceiver(pipe ot.IO, ch chan interface{}, bit bool) {
 		return
 	}
 
-	xb, err := FxReceive(oti, bit)
+	xb, err := FxkReceive(oti, b)
 	if err != nil {
 		ch <- err
 		return
