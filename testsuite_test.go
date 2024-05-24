a52599edb6a4c80a0f1e35fefc74f87b1e08d7f7
Markku Rossi <mtr@iki.fi>
2024-05-24 06:52:43 +0200

    Moved testsuite to toplevel.

--- testsuite_test.go
+++ testsuite_test.go
@@ -1,10 +1,10 @@
 //
-// Copyright (c) 2019-2023 Markku Rossi
+// Copyright (c) 2019-2024 Markku Rossi
 //
 // All rights reserved.
 //
 
-package compiler
+package mpc
 
 import (
 	"fmt"
@@ -18,14 +18,14 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/markkurossi/mpc"
 	"github.com/markkurossi/mpc/circuit"
+	"github.com/markkurossi/mpc/compiler"
 	"github.com/markkurossi/mpc/compiler/utils"
 	"github.com/markkurossi/mpc/types"
 )
 
 const (
-	testsuite = "tests"
+	testsuite = "testsuite"
 )
 
 var (
@@ -35,7 +35,7 @@ var (
 func TestSuite(t *testing.T) {
 	params := utils.NewParams()
 	params.MPCLCErrorLoc = true
-	compiler := New(params)
+	compiler := compiler.New(params)
 
 	filepath.WalkDir(testsuite,
 		func(path string, d fs.DirEntry, err error) error {
@@ -50,7 +50,7 @@ func TestSuite(t *testing.T) {
 		})
 }
 
-func testFile(t *testing.T, compiler *Compiler, file string) {
+func testFile(t *testing.T, compiler *compiler.Compiler, file string) {
 	if !strings.HasSuffix(file, ".mpcl") {
 		return
 	}
@@ -166,8 +166,8 @@ func testFile(t *testing.T, compiler *Compiler, file string) {
 		}
 		for idx := range results {
 			out := circ.Outputs[idx]
-			rr := mpc.Result(results[idx], out)
-			re := mpc.Result(outputs[idx], out)
+			rr := Result(results[idx], out)
+			re := Result(outputs[idx], out)
 
 			if !reflect.DeepEqual(rr, re) {
 				if out.Type.Type == types.TArray &&
