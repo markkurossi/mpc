c812ead3eec0e5fb7855406469c0bbc74cdb4c24
Markku Rossi <mtr@iki.fi>
2025-11-30 13:38:10 +0100

    Updated documentation.

--- ot/ot.go
+++ ot/ot.go
@@ -8,7 +8,11 @@
 // Package ot implements oblivious transfer protocols.
 package ot
 
-// OT defines Oblivious Transfer protocol.
+// OT defines the base 1-out-of-2 Oblivious Transfer protocol. The
+// sender uses the Send function to send a []Wire array where each
+// wire has zero and one Label. The receiver calls Receive with a
+// []bool array of selection bits. The higher level protocol must
+// ensure the []Wire and []bool array lengths match.
 type OT interface {
 	// InitSender initializes the OT sender.
 	InitSender(io IO) error
