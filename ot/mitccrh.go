241d54857a9fb4d80440b310fd8081c107e1009b
Markku Rossi <mtr@iki.fi>
2026-01-08 07:35:06 +0100

    Initial work on multi-instance TCCR hash (MITCCRH).

---
+++ ot/mitccrh.go
@@ -0,0 +1,135 @@
+//
+// Copyright (c) 2026 Markku Rossi
+//
+// All rights reserved.
+//
+
+/*
+
+This implementation is derived from the EMP Toolkit's mitccrh.h
+(https://github.com/emp-toolkit/emp-tool/blob/master/emp-tool/utils/mitccrh.h)
+with original license as follows:
+
+MIT License
+
+Copyright (c) 2018 Xiao Wang (wangxiao1254@gmail.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+Enquiries about further applications and development opportunities are welcome.
+
+*/
+
+package ot
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/binary"
+)
+
+const BlockSize = 16
+
+type Block [BlockSize]byte
+
+type MITCCRH struct {
+	batchSize int
+
+	startPoint Block
+	gid        uint64
+
+	keys    []Block
+	ciphers []cipher.Block
+	keyUsed int
+}
+
+func NewMITCCRH(s Block, batchSize int) *MITCCRH {
+	return &MITCCRH{
+		batchSize:  batchSize,
+		startPoint: s,
+		keys:       make([]Block, batchSize),
+		ciphers:    make([]cipher.Block, batchSize),
+		keyUsed:    batchSize, // force renew on first use
+	}
+}
+
+func (m *MITCCRH) renewKeys() {
+	for i := 0; i < m.batchSize; i++ {
+		tweak := makeBlock(m.gid, 0)
+		m.gid++
+
+		m.keys[i] = xorBlock(m.startPoint, tweak)
+
+		block, err := aes.NewCipher(m.keys[i][:])
+		if err != nil {
+			panic(err)
+		}
+		m.ciphers[i] = block
+	}
+	m.keyUsed = 0
+}
+
+func (m *MITCCRH) Hash(blks []Block, K, H int) {
+	if K > m.batchSize {
+		panic("K > batchSize")
+	}
+	if m.batchSize%K != 0 {
+		panic("batchSize % K != 0")
+	}
+	if K*H != len(blks) {
+		panic("K*H != len(blks)")
+	}
+	if m.keyUsed == m.batchSize {
+		m.renewKeys()
+	}
+
+	// tmp = blks
+	tmp := make([]Block, len(blks))
+	copy(tmp, blks)
+
+	// ParaEnc<K,H>
+	for k := 0; k < K; k++ {
+		c := m.ciphers[m.keyUsed+k]
+		for h := 0; h < H; h++ {
+			idx := k*H + h
+			c.Encrypt(tmp[idx][:], tmp[idx][:])
+		}
+	}
+	m.keyUsed += K
+
+	// blks ^= tmp
+	for i := range blks {
+		for j := 0; j < BlockSize; j++ {
+			blks[i][j] ^= tmp[i][j]
+		}
+	}
+}
+
+func xorBlock(a, b Block) (out Block) {
+	for i := 0; i < BlockSize; i++ {
+		out[i] = a[i] ^ b[i]
+	}
+	return
+}
+
+func makeBlock(hi, lo uint64) (b Block) {
+	binary.LittleEndian.PutUint64(b[0:8], hi)
+	binary.LittleEndian.PutUint64(b[8:16], lo)
+	return
+}
---
+++ ot/mitccrh_test.go
@@ -0,0 +1,61 @@
+//
+// Copyright (c) 2026 Markku Rossi
+//
+// All rights reserved.
+//
+
+package ot
+
+import (
+	"bytes"
+	"encoding/hex"
+	"testing"
+)
+
+var mitccrhTests = []struct {
+	key    string
+	blocks []string
+}{
+	{
+		key: "00000000000000000000000000000000",
+		blocks: []string{
+			"66e94bd4ef8a2c3b884cfa59ca342b2e",
+			"dc0ed85df9611abb7249cdd168c5467e",
+			"c117d2238d53836acd92ddcdb85d6a21",
+			"79c86d43f2be7fce99dd2c2133b0cf7c",
+			"dbe01de67e346a800c4c4b4880311de4",
+			"54ca53bb28791846e6b09a2757f014e4",
+			"86495e4a9c80564982f41de01f2b9884",
+			"d83636687394ca5538a73a2198ea4ab7",
+		},
+	},
+}
+
+func TestMITCCRH(t *testing.T) {
+	const (
+		batchSize = 8
+		k         = 8
+		h         = 2
+	)
+	for idx, test := range mitccrhTests {
+		var s Block
+		mitccrh := NewMITCCRH(s, batchSize)
+
+		blks := make([]Block, k*h)
+		mitccrh.Hash(blks, k, h)
+
+		for i := 0; i < k; i++ {
+			for j := 0; j < h; j++ {
+				expected, err := hex.DecodeString(test.blocks[i])
+				if err != nil {
+					t.Fatal(err)
+				}
+				result := blks[i*h+j]
+				if !bytes.Equal(expected, result[:]) {
+					t.Errorf("test-%d: %02d,%02d: %x != %x\n",
+						idx, i, j, expected, result)
+				}
+			}
+		}
+	}
+}
