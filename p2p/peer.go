15815e7fd854ca6ee9e22492890055810b73eeb1
Markku Rossi <mtr@iki.fi>
2026-03-23 15:40:12 +0200

    Updated documentation.

--- p2p/peer.go
+++ p2p/peer.go
@@ -21,12 +21,14 @@ func (p *Peer) String() string {
 	return fmt.Sprintf("%d[%v]", p.ID, p.Addr)
 }
 
+// Close closes all peer connections.
 func (p *Peer) Close() {
 	for _, conn := range p.Conns {
 		conn.Close()
 	}
 }
 
+// SetConn assigns conn to connID.
 func (p *Peer) SetConn(connID int, conn *Conn) error {
 	if len(p.Conns) <= connID {
 		n := make([]*Conn, connID+1)
