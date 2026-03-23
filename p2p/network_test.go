4e9ecce4ccda98158cb66f93358bf7df4e17534c
Markku Rossi <mtr@iki.fi>
2026-03-23 15:33:05 +0200

    Added protocol tests.

--- p2p/network_test.go
+++ p2p/network_test.go
@@ -10,12 +10,13 @@ package p2p
 
 import (
 	"fmt"
+	"sync"
 	"testing"
 )
 
 func TestNetwork(t *testing.T) {
 	port := 9000
-	numParties := 3
+	numParties := 5
 	numConns := 3
 	parties := make([]*Network, numParties)
 	done := make(chan error)
@@ -47,14 +48,91 @@ func TestNetwork(t *testing.T) {
 			t.Fatal(err)
 		}
 	}
+
+	var wg sync.WaitGroup
+
+	self := parties[0].Self
+
+	for _, peer := range parties[0].Peers {
+		if peer.ID == self.ID {
+			continue
+		}
+		for i, conn := range peer.Conns {
+			if self.ID < peer.ID {
+				wg.Go(func() {
+					ping(conn, self, peer, i)
+				})
+			} else {
+				wg.Go(func() {
+					pong(conn, self, peer, i)
+				})
+			}
+		}
+	}
+
+	wg.Wait()
 }
 
 func runParty(nw *Network, done chan error) {
-	done <- partyMain(nw)
+	if err := nw.Connect(); err != nil {
+		done <- err
+		return
+	}
+	done <- nil
+
+	self := nw.Self
+
+	var wg sync.WaitGroup
+
+	for _, peer := range nw.Peers {
+		if peer.ID == self.ID {
+			continue
+		}
+		for i, conn := range peer.Conns {
+			if self.ID < peer.ID {
+				wg.Go(func() {
+					ping(conn, self, peer, i)
+				})
+			} else {
+				wg.Go(func() {
+					pong(conn, self, peer, i)
+				})
+			}
+		}
+	}
+
+	wg.Wait()
 }
 
-func partyMain(nw *Network) error {
-	return nw.Connect()
+func ping(conn *Conn, from, to *Peer, connID int) {
+	req := "ping"
+	if err := conn.SendString("ping"); err != nil {
+		panic(err)
+	}
+	if err := conn.Flush(); err != nil {
+		panic(err)
+	}
+	resp, err := conn.ReceiveString()
+	if err != nil {
+		panic(err)
+	}
+	if resp != req {
+		panic(resp)
+	}
+}
+
+func pong(conn *Conn, from, to *Peer, connID int) {
+	msg, err := conn.ReceiveString()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("pong: from=%v, to=%v, conn=%v\n", from, to, connID)
+	if err := conn.SendString(msg); err != nil {
+		panic(err)
+	}
+	if err := conn.Flush(); err != nil {
+		panic(err)
+	}
 }
 
 func makeAddr(port int) string {
